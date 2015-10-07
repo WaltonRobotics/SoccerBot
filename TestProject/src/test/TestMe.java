ed9f957bab811051fb4c7b87d057f29e7ac81a51
Dean <dean.vookles@waltonrobotics.org>
2015-10-07 19:42:32 -0400

    Changed the message in the SOP.

--- TestProject/src/test/TestMe.java
+++ TestProject/src/test/TestMe.java
@@ -2,7 +2,7 @@ package test;
 public class TestMe {
 
 	public static void main(String[] args) {
-		System.out.println("Thule Hammer! Youtube!");
+		System.out.println("I CAN FEEL IT! I'M INSIIIIIIIIIIIIIDE!!!!");
 
 	}
 
