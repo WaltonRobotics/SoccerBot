44a90f7f14e34026fd6081de4d1ff4efedac156d
Piyush K <piyush.kancharlawar@waltonrobotics.org>
2015-11-14 11:31:38 -0500

    Fixed intake manual Added gamepad class - wrapper class of joystick

--- SoccerBot/src/org/usfirst/frc2974/SoccerBot/commands/IntakeManual.java
+++ SoccerBot/src/org/usfirst/frc2974/SoccerBot/commands/IntakeManual.java
@@ -1,68 +1,65 @@
 package org.usfirst.frc2974.SoccerBot.commands;
 
+import org.usfirst.frc2974.SoccerBot.Gamepad;
 import org.usfirst.frc2974.SoccerBot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc2974.SoccerBot.subsystems.*;;
+
 /**
  *
  */
 public class IntakeManual extends Command {
 
-    public IntakeManual() {
-    	super();
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.intake);
-    }
+	public IntakeManual() {
+		super();
+		
+		requires(Robot.intake);
+	}
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
- protected void execute() {
-    	
-    	boolean isPressed = Robot.oi.joystickLeft.getRawButton(BUTTON_UP);
-    	
-    	switch (Robot.intake.getAction())
-    	{
-    	case up:
-    		if(!isPressed)
-    		break;
-    		
-    	case fall:
-    		if(isPressed)
-    		break;
-    	
-    	if(Robot.oi.joystickLeft.getRawButton(BUTTON_UP))
-    	{
-    		Robot.intake.setArmMovement(Intake.ArmMovement.up);
-    	}
-    	else
-    	{
-    		Robot.intake.setArmMovement(Intake.ArmMovement.fall);
-    	}
-    	
-    	
-    	}
-    	
-    	
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
 
+		boolean isPressed = Robot.oi.xbox.getButton(Gamepad.Button.A);
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+		switch (Robot.intake.getAction()) {
+		//if not pressed and in up- do nothing
+		case up:
+			if (!isPressed)
+				break;
+		//if pressed and in fall - do nothing
+		case fall:
+			if (isPressed)
+				break;
+			//if pressed and in fall, go up
+			if (isPressed) {
+				Robot.intake.setArmMovement(Intake.ArmMovement.up);
+			} else {//if not pressed and in up, fall
+				Robot.intake.setArmMovement(Intake.ArmMovement.fall);
+			}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+		}
+		
+		Robot.intake.setMotorPower(Robot.oi.xbox.getY());
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
 }
