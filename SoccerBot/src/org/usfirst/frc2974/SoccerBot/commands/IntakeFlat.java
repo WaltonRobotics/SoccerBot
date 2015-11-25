7b1d06bd01ee4794b874941461c4f7309e5df710
Tim Stanistreet <tim.stanistreet@waltonrobotics.org>
2015-11-24 21:21:58 -0500

    Updated intakeflat so if intakeflat is initialized it will descend to the flat position

--- SoccerBot/src/org/usfirst/frc2974/SoccerBot/commands/IntakeFlat.java
+++ SoccerBot/src/org/usfirst/frc2974/SoccerBot/commands/IntakeFlat.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc2974.SoccerBot.commands;
 
 import org.usfirst.frc2974.SoccerBot.Robot;
+import org.usfirst.frc2974.SoccerBot.subsystems.Intake;
 import org.usfirst.frc2974.SoccerBot.subsystems.Intake.ArmMovement;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -16,12 +17,13 @@ public class IntakeFlat extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.intake.setMotorPower(0);
+    	Robot.intake.setArmMovement(Intake.ArmMovement.fall);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.intake.setMotorPower(0);
-    	Robot.intake.setArmMovement(ArmMovement.fall);
+    	//todo display value on smartdashbord to show how much the arm has fallen
     }
 
     // Make this return true when this Command no longer needs to run execute()
