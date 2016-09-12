6a4384acd07b8d7a3c13e977116fc9da0ecffb19
Piyush <piyushgk1@gmail.com>
2016-09-12 01:22:37 -0400

    removed autonomous command

--- SoccerBot/src/org/usfirst/frc2974/SoccerBot/Robot.java
+++ SoccerBot/src/org/usfirst/frc2974/SoccerBot/Robot.java
@@ -86,12 +86,6 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
 		SmartDashboard.putString("current intake command", intake.getCurrentCommand().toString());
 	}
 
